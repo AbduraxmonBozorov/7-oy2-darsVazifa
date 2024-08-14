88569ee2c06b6dfa2498a27ef65e434b49969351
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-08-14 16:00:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {lazy, Suspense, useEffect} from 'react'
+
+
+function App() {
+  useEffect(()=>{
+    
+  }, [])
+  return (
+    <div>
+      <h1>App component</h1>
+
+    </div>
+  )
+}
+
+export default App
